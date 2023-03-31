8d8ec2fc427ac588829ba9fc704bdd9fcb061b5d
Dog2077 <1002302241@qq.com>
2023-03-31 23:19:46 +0800

    Add introduction of Xi_BackEnd

--- apps/app01/settings.py
+++ apps/app01/settings.py
@@ -1,7 +1,12 @@
+# # 小程序开发者id
+# AppId = "wxbdacc2346f2dd78d"
+# # 小程序的AppSecret
+# AppSecret = "cb05acea99a4ac214647c2640925ddbf"
+
 # 小程序开发者id
-AppId = "wxbdacc2346f2dd78d"
+AppId = "wxab78d59648fbe853"
 # 小程序的AppSecret
-AppSecret = "cb05acea99a4ac214647c2640925ddbf"
+AppSecret = "74b314070a2dcf01404291ef42f6af3f"
 
 code2Session = "https://api.weixin.qq.com/sns/jscode2session?appid={}&secret={}&js_code={}&grant_type=authorization_code"
 pay_mchid = '...'
